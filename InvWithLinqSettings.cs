8c44c60144306794531a0a9745e75960c7f8d0de
Sean <sean.daxxtrias@gmail.com>
2025-01-04 16:55:12 -0600

    add support for stash filtering

--- InvWithLinqSettings.cs
+++ InvWithLinqSettings.cs
@@ -10,6 +10,7 @@ namespace InvWithLinq;
 public class InvWithLinqSettings : ISettings
 {
     public ToggleNode RunOutsideTown { get; set; } = new ToggleNode(true);
+    public ToggleNode EnableForStash { get; set; } = new ToggleNode(true);
     public ToggleNode Enable { get; set; } = new ToggleNode(false);
     public ColorNode FrameColor { get; set; } = new ColorNode(Color.Red);
     public RangeNode<int> FrameThickness { get; set; } = new RangeNode<int>(1, 1, 20);
